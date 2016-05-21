f6141d2a794b5c81349f033b77bda10d9c59de1a
Aldo Ziflaj <aldoziflaj95@gmail.com>
2016-05-21 09:57:22 +0000

    add logged in user to session

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = "Successfully logged in"
+      redirect_to user_path(user)
+    else
+      flash.now[:danger] = "Incorrect email or password"
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "Successfully logged out"
+    redirect_to root_path
+  end
+end
