50bb0bccee3a69f784d00bbe13647771ca946723
Aldo Ziflaj <aldoziflaj95@gmail.com>
2016-05-21 20:27:29 +0000

    login on signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,8 +13,9 @@ class UsersController < ApplicationController
   def create
     @user = User.create(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = "Welcome to AlphaBlog #{@user.username}"
-      redirect_to articles_path
+      redirect_to user_path(@user)
     else
       render 'new'
     end
