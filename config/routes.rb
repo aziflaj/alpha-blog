406e621f20f7346d2f0d8d312f6dbb113462d09a
Aldo Ziflaj <aldoziflaj95@gmail.com>
2016-05-22 10:31:26 +0000

    test categories controller

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
   post 'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
 
+  resources :categories, except: [:destroy]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
