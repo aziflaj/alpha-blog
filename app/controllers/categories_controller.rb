852d4b8d47f1338f3697fbd897ed3da5635a8945
Aldo Ziflaj <aldoziflaj95@gmail.com>
2016-05-23 09:12:03 +0000

    edit category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -24,6 +24,20 @@ class CategoriesController < ApplicationController
     @category_articles = @category.articles.paginate(page: params[:page], per_page: 5)
   end
 
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:success] = "#{@category.name} was successfully updated"
+      redirect_to category_path(@category)
+    else
+      render 'edit'
+    end
+  end
+
   private
     def category_params
       params.require(:category).permit(:name)
